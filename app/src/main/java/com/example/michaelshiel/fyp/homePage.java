9cb08209e2e9aa2010a0067a2029366a1ba4c30b
shielmi <shielmi@tcd.ie>
2018-03-03 20:48:06 +0000

    Updates 3/3/18

--- app/src/main/java/com/example/michaelshiel/fyp/homePage.java
+++ app/src/main/java/com/example/michaelshiel/fyp/homePage.java
@@ -72,10 +72,10 @@ public class homePage extends AppCompatActivity {
         mLunchReference = mFirebaseDatabase.getReference().child("lunch");
         mDinnerReference = mFirebaseDatabase.getReference().child("dinner");
         mTescoPricesReference = mFirebaseDatabase.getReference().child("tescoPrices");
-        mSupervaluPricesReference = mFirebaseDatabase.getReference().child("supervalu prices");
-        mAldiReference = mFirebaseDatabase.getReference().child("aldi prices");
+        mSupervaluPricesReference = mFirebaseDatabase.getReference().child("supervaluPrices");
+        mAldiReference = mFirebaseDatabase.getReference().child("aldiPrices");
         mUserReference = mFirebaseDatabase.getReference().child("user");
-        mGroceryListReference = mFirebaseDatabase.getReference().child("grocery list");
+        mGroceryListReference = mFirebaseDatabase.getReference().child("groceryList");
         new doit().execute();
 
 
@@ -87,12 +87,13 @@ public class homePage extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                String url = "https://www.tesco.ie/groceries/product/browse/default.aspx?N=4294954026&Ne=4294954028";
+                String url = "https://shop.supervalu.ie/shopping/meat-poultry-steaks-chops/c-150300405";
                 Document document = Jsoup.connect(url).get();
 
-                //OrderedList(document, url);
-                //UnorderedList(document, url);
-                TescoPrices(document, url);
+                //getRecipe(document, url);
+                //TescoPrices(document, url);
+                SupervaluPrices(document, url);
+                //AldiPrices(document, url);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -111,7 +112,9 @@ public class homePage extends AppCompatActivity {
           //  mTescoPricesReference.push().setValue(price);
 
         }
-        public void OrderedList( Document document, String url) throws IOException
+
+
+        public void getRecipe( Document document, String url) throws IOException
         {
             mIngredients = new ArrayList();
             mInstructions = new ArrayList();
@@ -141,123 +144,7 @@ public class homePage extends AppCompatActivity {
             Elements fat = document.select("[itemprop=fatContent]");
             mFat = fat.text();
 
-
-
-//            Elements ingredients = document.getElementsByClass("recipe-check-list").first().getElementsByTag("li");
-//            for (Element el : ingredients) {
-//                List<String> singleRecipe = new ArrayList<>();
-//                Element recipe3 = el.select("[itemprop=recipeIngredient]").first();
-//                //mRecipe.add(recipe3.text());
-//                String test = recipe3.text();
-//                Scanner scan = new Scanner(test);
-//                scan.useDelimiter(", ");
-//                while(scan.hasNext())
-//                {
-//                    String s = scan.next();
-//                    if (s.matches(".*\\d+.*"))
-//                    {
-//                        String st = extractNumber(s);
-//                        String amount = extractServing(s);
-//                        singleRecipe.add(st);
-//                        singleRecipe.add(amount);
-//                    }
-//                    else
-//                    {
-//                        singleRecipe.add(s);
-//                    }
-//                }
-//                mIngredients.add(singleRecipe);
-//
-//            }
-
-            Elements ingredients = document.getElementsByClass("recipe-check-list").first().getElementsByTag("li");
-            for (Element el : ingredients) {
-                List singleRecipe = new ArrayList<>();
-                List<String> amounts = new ArrayList<>();
-                Element recipe3 = el.select("[itemprop=recipeIngredient]").first();
-
-                String test = recipe3.text();
-                Scanner scan = new Scanner(test);
-                int i=0;
-                if(scan.hasNext() &&  i==0)
-                {
-                    String s = scan.next();
-                    if (s.matches(".*\\d+.*"))
-                    {
-                        String st = extractNumber(s);
-
-                        String amount = extractServing(scan.next());
-                        amounts.add(amount);
-                        amounts.add(st);
-
-                    }
-                }
-                String rest = "";
-                while(scan.hasNext())
-                {
-                    String temp = scan.next();
-                    if(rest.equals("")==false)
-                    {
-                        rest = rest.concat(" " + temp);
-                    }
-                    else
-                    {
-                        rest = rest.concat( temp);
-                    }
-
-                }
-                singleRecipe.add(rest);
-                singleRecipe.add(amounts);
-            }
-
-            Elements instructions = document.getElementsByTag("ol").first().getElementsByTag("li");
-            for (Element el : instructions) {
-
-                Element recipe4 = el.getElementsByTag("li").first();
-                mInstructions.add(recipe4.text());
-
-            }
-
-            Elements yield = document.select("[itemprop=recipeYield]").first().getElementsByTag("p");
-            mSurvingSuggestion = yield.text();
-
-            Elements author =  document.getElementsByClass("aboutAuthor").first().getElementsByClass("name");
-            mWriter =  author.text();
-
-            mUrl = url;
-        }
-
-        public void UnorderedList( Document document, String url) throws IOException
-        {
-            mIngredients = new ArrayList();
-            mInstructions = new ArrayList();
-            name = document.title();
-
-//            Elements prepTime = document.getElementsByClass("recipe-time-bar left").first().getElementsByClass("info");
-//            mPrepTime = prepTime.text();
-//
-//            Elements cookTime = document.getElementsByClass("recipe-time-bar right").first().getElementsByClass("info");
-//            mCookTime = cookTime.text();
-            mPrepTime = null;
-            mCookTime = null;
-
-            Elements calories = document.getElementsByClass("field field-name-field-recipe-calories field-type-text field-label-hidden");
-            mCalories = calories.text();
-
-
-            Elements protein = document.select("[itemprop=proteinContent]");
-            Element measure = document.getElementsByClass("amount").first();
-            mProtein = protein.text();
-            mMeasurement = measure.text();
-
-            Elements carbs = document.select("[itemprop=carbohydrateContent]");
-            mCarbs = carbs.text();
-
-
-            Elements fat = document.select("[itemprop=fatContent]");
-            mFat = fat.text();
-
-
+            //When amounts were at the end
 //            Elements ingredients = document.getElementsByClass("recipe-check-list").first().getElementsByTag("li");
 //            for (Element el : ingredients) {
 //               List<String> singleRecipe = new ArrayList<>();
@@ -285,6 +172,7 @@ public class homePage extends AppCompatActivity {
 //
 //            }
 
+            //When amounts were at the start
 //            Elements ingredients = document.getElementsByClass("recipe-check-list").first().getElementsByTag("li");
 //            for (Element el : ingredients) {
 //                List singleRecipe = new ArrayList<>();
@@ -336,12 +224,22 @@ public class homePage extends AppCompatActivity {
 //                mIngredients.add(singleRecipe);
 //            }
 
+            //Unordered List
             Elements instructions = document.select("[itemprop=recipeInstructions]").first().getElementsByTag("p");
             for (Element el : instructions)
             {
                 Element recipe4 = el.getElementsByTag("p").first();
                 mInstructions.add(recipe4.text());
             }
+
+            //Ordered List
+//            Elements instructions = document.getElementsByTag("ol").first().getElementsByTag("li");
+//            for (Element el : instructions) {
+//
+//                Element recipe4 = el.getElementsByTag("li").first();
+//                mInstructions.add(recipe4.text());
+//
+//            }
 
             Elements yield = document.select("[itemprop=recipeYield]").first().getElementsByTag("p");
             mSurvingSuggestion = yield.text();
@@ -360,9 +258,6 @@ public class homePage extends AppCompatActivity {
                 if (el.getElementsByClass("linePrice").first() != null)
                 {
                     mAmount = new ArrayList<>();
-                    List amounts = new ArrayList<>();
-                    String weight = null;
-                    //outputStream.println("		\"price" + i + "\": {");
                     Element names = el.getElementsByTag("a").first();
                     name = names.text();
                     int amount = 0;
@@ -393,7 +288,7 @@ public class homePage extends AppCompatActivity {
                                 }
                                 else if(amount>10)
                                 {
-                                    mWeight = "G";
+                                    mWeight = "Grams";
                                 }
                             }
 
@@ -405,7 +300,7 @@ public class homePage extends AppCompatActivity {
                         }
                         else if(amount>10)
                         {
-                            mWeight = "G";
+                            mWeight = "Grams";
                         }
                     }
                     Element linePrice = el.getElementsByClass("linePrice").first();
@@ -422,12 +317,102 @@ public class homePage extends AppCompatActivity {
                     TescoPrice price = new TescoPrice(name,mAmount,mCurrency, mPrice);
                     mTescoPricesReference.push().setValue(price);
                 }
-
             }
-
-
-
         }
+
+        public void SupervaluPrices(Document document, String url){
+            Elements info = document.getElementsByClass("row product-list ga-impression-group").first().getElementsByClass("product-list-item-details-title-link ga-product-link");
+            int i = 0;
+            for (Element el : info) {
+                mAmount = new ArrayList<>();
+                Element names = el.getElementsByClass("product-list-item-details-title").first();
+                Element linePrice = el.getElementsByClass("product-details-price-item").first();
+                Element linePriceAbbrv = el.getElementsByClass("product-details-price-per-kg").first();
+                String fullPrice = linePrice.text();
+                mCurrency = "€";
+                mPrice = extractNumber(fullPrice);
+                name = names.text();
+                String amount = null;
+                String serving = null;
+                if (name.contains("(")) {
+                    String[] values = name.split("[\\(||\\)]");
+                    if (values[1] != null) {
+                        String amountString = values[1];
+                        if (amountString.matches(".*\\d+.*")) {
+                            amount = extractNumber(amountString);
+                            mWeight = extractServing(amountString);
+                        }
+                    }
+                } else if (name.contains("Loose")) {
+                    amount = "1";
+                } else if (linePriceAbbrv.text().equals("per kg")) {
+                    amount = "1";
+                    mWeight = "KG";
+                }
+                mAmount.add(amount);
+                if(mWeight!=null)
+                {
+                    mAmount.add(mWeight);
+                }
+                TescoPrice price = new TescoPrice(name,mAmount,mCurrency, mPrice);
+                mSupervaluPricesReference.push().setValue(price);
+            }
+        }
+
+        public void AldiPrices(Document document, String url){
+            Elements info = document.getElementsByClass("col-xs-12 col-sm-12 category-grid category-grid--groceries").first().getElementsByClass(" category-item invisible js-category-item");;
+            int i = 0;
+            for (Element el : info) {
+                mAmount = new ArrayList<>();
+                Element names = el.getElementsByClass("category-item__title").first();
+                Element linePrice = el.getElementsByClass("category-item__price js-category-item-price").first();
+                Element linePriceAbbrv = el.getElementsByClass("category-item__pricePerUnit").first();
+                String fullPrice = linePrice.text();
+                String perKg = linePriceAbbrv.text();
+                double amount=0;
+                double x = 0;
+                String priceNumber=null;
+                if( perKg.equals("")==false)
+                {
+                    if(perKg.contains("/"))
+                    {
+                        Scanner scan= new Scanner(perKg);
+                        scan.useDelimiter("/");
+                        if(scan.hasNext())
+                        {
+                            perKg = scan.next();
+                        }
+                    }
+                    Scanner scan = new Scanner(fullPrice);
+                    scan.useDelimiter("€");
+                    String scanned = scan.next();
+                    priceNumber = extractNumber(scanned);
+                    String perKgNo = extractNumber(perKg);
+                    x = Double.parseDouble(priceNumber);
+                    double y = Double.parseDouble(perKgNo);
+                    y= y/1000;
+                    amount = x/y;
+                    double mweight = amount - (int) amount;
+                    if(mweight>=.5)
+                    {
+                        amount = Math.ceil(amount);
+                    }
+                    else{
+                        amount = (int) amount;
+                    }
+
+                }
+                mPrice = "" + x;
+                name = names.text();
+                mWeight = "Grams";
+                mCurrency = "€";
+                mAmount.add(amount);
+                mAmount.add(mWeight);
+                TescoPrice price = new TescoPrice(name,mAmount,mCurrency, mPrice);
+                mAldiReference.push().setValue(price);
+            }
+        }
+
 
         public String extractNumber(final String str) {
 
